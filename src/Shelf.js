794da353c5b5d5697d0122f25d70fac8163847bd
SM <sean.martin@gmail.com>
2017-10-01 15:59:44 -0500

    add ChangeShelf to Shelf

--- src/Shelf.js
+++ src/Shelf.js
@@ -22,19 +22,7 @@ class Shelf extends React.Component {
                       'url("http://books.google.com/books/content?id=yDtCuFHXbAYC&printsec=frontcover&img=1&zoom=1&imgtk=AFLRE72RRiTR6U5OUg3IY_LpHTL2NztVWAuZYNFE8dUuC0VlYabeyegLzpAnDPeWxE6RHi0C2ehrR9Gv20LH2dtjpbcUcs8YnH5VCCAH0Y2ICaKOTvrZTCObQbsfp4UbDqQyGISCZfGN&source=gbs_api")'
                     }}
                   />
-                  <div className="book-shelf-changer">
-                    <select>
-                      <option value="none" disabled>
-                        Move to...
-                                </option>
-                      <option value="currentlyReading">
-                        Currently Reading
-                                </option>
-                      <option value="wantToRead">Want to Read</option>
-                      <option value="read">Read</option>
-                      <option value="none">None</option>
-                    </select>
-                  </div>
+                  <ChangeShelf />
                 </div>
                 <div className="book-title">Enders Game</div>
                 <div className="book-authors">Orson Scott Card</div>
